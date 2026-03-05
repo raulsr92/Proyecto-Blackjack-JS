3545ef84c90880f9e089daddae52229110bf3d7c
Raul Sanchez R. <raul.sr92@gmail.com>
2026-03-05 17:35:53 -0500

    Clase 50 Crear baraja de cartas

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,1 +1,51 @@
 
+/*
+ 2C = Two of Clubs (Tréboles)
+ 2D = Two of Diamonds (Diamantes o rombos)
+ 2H = Two of Hearts (Corazones)
+ 2S = Two of Spades (Espadas)
+
+*/
+
+/*Variables, arreglos*/
+
+let deck = [];
+const tipos = ['C','D', 'H', 'S']
+const especiales = ['A','J', 'Q', 'K']
+
+/*Funciones*/
+
+const crearDeck = () =>{
+
+    console.warn("Creación de la baraja")
+
+    /*Creación de baraja del 1 al 10*/ 
+
+    for (let i = 2; i < 11; i++) {
+
+        for (let j= 0; j < tipos.length; j++) {
+
+            deck.push(`${i}${tipos[j]}`)
+        }
+    }
+
+    /*Creación de baraja de A, J, Q y K */
+
+    for (const especial of especiales) {
+
+        for (const tipo of tipos) {
+            
+            deck.push(`${especial}${tipo}`)
+        }
+        
+    }
+    console.log({deck})
+    deck = _.shuffle(deck)
+    console.log({deck})
+
+    return deck
+}
+
+
+let miBaraja =crearDeck()
+console.log(miBaraja)
